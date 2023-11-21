e746fbcfaa1551dbcdb59595bc0c0c47d71b6936
AdriELTE <ka@inf.elte.hu>
2023-11-21 21:29:10 +0100

    Átírtam a galéria nevét

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Title = () => {
   return (
     <div className="title">
-      <h1>FireGram</h1>
+      <h1>DoggyGram</h1>
       <h2>Your Pictures</h2>
       <p>You can upload your images!</p>
     </div>
